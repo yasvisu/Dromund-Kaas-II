f2afd58fa6ef010bd6f3075c059105055e06198b
Ivaylo Hristov <hristofff@yahoo.com>
2015-06-16 21:29:22 +0300

    Added Splash Screen, Game Screen, Screen Manager

---
+++ DromundKaasII/DromundKaas/Graphics/ScreenManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DromundKaasII
+{
+    public class ScreenManager
+    {
+        private static ScreenManager instance;
+        public Vector2 Dimensions { private set; get; }
+        public ContentManager Content { private set; get; }
+
+        GameScreen currentScreen;
+
+        public static ScreenManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ScreenManager();
+                }
+                return instance;
+            }
+        }
+
+        public ScreenManager()
+        {
+            Dimensions = new Vector2(640, 480);
+            currentScreen = new SplashScreen();
+        }
+
+        public void LoadContent(ContentManager Content)
+        {
+            this.Content = new ContentManager(Content.ServiceProvider, "Content");
+            currentScreen.LoadContent();
+        }
+
+        public void UnloadContent()
+        {
+            currentScreen.UnloadContent();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            currentScreen.Update(gameTime);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            currentScreen.Draw(spriteBatch);
+        }
+    }
+}
---
+++ DromundKaasII/DromundKaas/Graphics/SplashScreen.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DromundKaasII
+{
+    class SplashScreen : GameScreen
+    {
+        Texture2D image;
+        string path;
+
+        public override void LoadContent()
+        {
+            base.LoadContent();
+            path = "SplshScreen/image1.png";
+            image = content.Load<Texture2D>(path);
+        }
+
+        public override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(image, Vector2.Zero, Color.White);
+        }
+    }
+}

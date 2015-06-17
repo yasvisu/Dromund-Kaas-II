9d38e73513075b60fabcc752e8f8eb5c870050f4
yasvisu <yasvisu@gmail.com>
2015-06-17 15:12:57 +0300

    Added Player class implementing IPlayer between Actor and the player professions.

---
+++ DromundKaasII/DromundKaas/GameObjects/Actors/Players/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DromundKaasII.Interfaces;
+
+namespace DromundKaasII.GameObjects.Actors.Players
+{
+    class Player : Actor, IPlayer
+    {
+        public int Score
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}

95ed0605e8a0abe145e791d620c6781b23330df9
yasvisu <yasvisu@gmail.com>
2015-06-17 15:18:13 +0300

    Added Fighter constructor.

--- DromundKaasII/DromundKaas/GameObjects/Actors/Players/Tier1/Fighter.cs
+++ DromundKaasII/DromundKaas/GameObjects/Actors/Players/Tier1/Fighter.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace DromundKaasII.GameObjects.Actors.Players.Tier1
 {
     class Fighter : Player
     {
+        public Fighter(Vector2 MapPosition)
+            : base(MapPosition)
+        {
+
+        }
     }
 }

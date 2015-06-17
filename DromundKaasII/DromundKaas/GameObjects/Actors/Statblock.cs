9ab0adf6a761afaa0232919a265db4de02177d4d
yasvisu <yasvisu@gmail.com>
2015-06-17 14:45:12 +0300

    Adding secondary and primary attributes to statblock.

--- DromundKaasII/DromundKaas/GameObjects/Actors/Statblock.cs
+++ DromundKaasII/DromundKaas/GameObjects/Actors/Statblock.cs
@@ -7,15 +7,24 @@ namespace DromundKaasII.GameObjects.Actors
 {
     public class Statblock
     {
+        // Combat
         public int Health { get; set; }
         public int Mana { get; set; }
         public int Focus { get; set; }
 
+        // Experience
+        public int Experience { get; set; }
+
+        // Primary stats
         public float Strength { get; set; }
+        public float Dexterity { get; set; }
         public float Constitution { get; set; }
         public float Intelligence { get; set; }
-        public float Dexterity { get; set; }
-        // ...
+
+        // Secondary stats
+        public float Wisdom { get; set; }
+        public float Charisma { get; set; }
+        public float Psychic { get; set; }
 
     }
 }

c9ee77a3c448b128838557b879792fd7086f7bb5
yasvisu <yasvisu@gmail.com>
2015-06-17 14:42:15 +0300

    Added some fields to Skill.cs

--- DromundKaasII/DromundKaas/GameObjects/Skills/Skill.cs
+++ DromundKaasII/DromundKaas/GameObjects/Skills/Skill.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using DromundKaasII.GameObjects.Actors;
+using Microsoft.Xna.Framework;
 
 namespace DromundKaasII.GameObjects.Skills
 {
-    class Skill
+    public class Skill
     {
+        string name;
+        int manaCost;
+        int focusCost;
+
+        Vector2 mapTarget;
+        Actor actorTarget;
+
+
     }
 }

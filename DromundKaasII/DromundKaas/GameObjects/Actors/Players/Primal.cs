09cde5f6c546111c91f8417d22d29e5c4f887861
yasvisu <yasvisu@gmail.com>
2015-06-17 14:59:37 +0300

    Removed base stat functionality. Max combat stats will now be calculated based on Level and primary attribute.

--- DromundKaasII/DromundKaas/GameObjects/Actors/Players/Primal.cs
+++ DromundKaasII/DromundKaas/GameObjects/Actors/Players/Primal.cs
@@ -8,10 +8,7 @@ namespace DromundKaasII.GameObjects.Actors.Players
 {
     class Primal : Actor
     {
-        private const int BaseHealth = 100;
-        private const int BaseMana = 100;
-        private const int BaseFocus = 100;
-        
+     
         public Primal(Vector2 MapPosition, int baseHealth, int baseMana, int baseFocus):base(MapPosition)
         {
             this.Stats = new Statblock();

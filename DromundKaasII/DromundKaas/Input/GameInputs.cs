dda6fe9cddd3e330a3a1fdb64da576273f9d6485
yasvisu <yasvisu@gmail.com>
2015-06-18 11:28:49 +0300

    #7 Added enum GameInputs to hold the logical types of input, separate from input mode.

---
+++ DromundKaasII/DromundKaas/Input/GameInputs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DromundKaasII.Input
+{
+    public enum GameInputs
+    {
+        // Movement
+
+        Up,
+        Down,
+        Left,
+        Right,
+
+        // Actions
+
+        A1,
+        A2,
+        A3,
+        A4,
+        A5,
+
+        Interact,
+
+        // Menus
+
+        Pause,
+
+        Quit
+    }
+}

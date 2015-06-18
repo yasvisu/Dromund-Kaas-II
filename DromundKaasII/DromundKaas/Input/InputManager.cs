3d1dd3d4eb4f679e20d5ccbb25f50b195a71f3a9
yasvisu <yasvisu@gmail.com>
2015-06-18 11:21:09 +0300

    #7 Added logic and UpdateInput() method to InputManager

--- DromundKaasII/DromundKaas/Input/InputManager.cs
+++ DromundKaasII/DromundKaas/Input/InputManager.cs
@@ -3,14 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
 
 namespace DromundKaasII.Input
 {
     public class InputManager
     {
+        public InputManager()
+        {
+            this.InputMode = InputModes.Keyboard;
+            this.KeyboardScheme = new KeyboardInputScheme();
+            this.GamePadScheme = new GamePadInputScheme();
+            this.CurrentKeyboard = Keyboard.GetState();
+            this.CurrentPlayerIndex = PlayerIndex.One;
+            this.CurrentGamePad = GamePad.GetState(CurrentPlayerIndex);
+        }
+
+        public InputModes InputMode { get; set; }
+
+        public KeyboardInputScheme KeyboardScheme { get; set; }
+        public KeyboardState CurrentKeyboard { get; set; }
+
+        public GamePadInputScheme GamePadScheme { get; set; }
+        public GamePadState CurrentGamePad { get; set; }
+        public PlayerIndex CurrentPlayerIndex { get; set; }
+
         public void UpdateInput()
         {
-
+            if(InputMode==InputModes.Keyboard)
+            {
+                CurrentKeyboard = Keyboard.GetState();
+            }
+            else if(InputMode==InputModes.GamePad)
+            {
+                CurrentGamePad = GamePad.GetState(CurrentPlayerIndex);
+            }
         }
     }
 }

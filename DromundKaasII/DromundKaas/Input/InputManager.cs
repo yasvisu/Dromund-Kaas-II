d18f0a6d1b724aebc12ba1750870b01f1906542d
yasvisu <yasvisu@gmail.com>
2015-06-18 11:42:25 +0300

    #7 Added logic to InputManager.UpdaeInput()

--- DromundKaasII/DromundKaas/Input/InputManager.cs
+++ DromundKaasII/DromundKaas/Input/InputManager.cs
@@ -30,14 +30,151 @@ namespace DromundKaasII.Input
 
         public void UpdateInput()
         {
-            if(InputMode==InputModes.Keyboard)
+            if (InputMode == InputModes.Keyboard)
             {
                 CurrentKeyboard = Keyboard.GetState();
             }
-            else if(InputMode==InputModes.GamePad)
+            else if (InputMode == InputModes.GamePad)
             {
                 CurrentGamePad = GamePad.GetState(CurrentPlayerIndex);
             }
+        }
+
+        public bool IsPressed(GameInputs Input)
+        {
+            switch (Input)
+            {
+                case GameInputs.Up:
+                    if (InputMode == InputModes.Keyboard)
+                    {
+                        return CurrentKeyboard.IsKeyDown(KeyboardScheme.Up);
+                    }
+                    else if (InputMode == InputModes.GamePad)
+                    {
+                        return CurrentGamePad.IsButtonDown(GamePadScheme.Up);
+                    }
+                    break;
+                case GameInputs.Down:
+                    if (InputMode == InputModes.Keyboard)
+                    {
+                        return CurrentKeyboard.IsKeyDown(KeyboardScheme.Down);
+                    }
+                    else if (InputMode == InputModes.GamePad)
+                    {
+                        return CurrentGamePad.IsButtonDown(GamePadScheme.Down);
+                    }
+                    break;
+                case GameInputs.Left:
+                    if (InputMode == InputModes.Keyboard)
+                    {
+                        return CurrentKeyboard.IsKeyDown(KeyboardScheme.Left);
+                    }
+                    else if (InputMode == InputModes.GamePad)
+                    {
+                        return CurrentGamePad.IsButtonDown(GamePadScheme.Left);
+                    }
+                    break;
+                case GameInputs.Right:
+                    if (InputMode == InputModes.Keyboard)
+                    {
+                        return CurrentKeyboard.IsKeyDown(KeyboardScheme.Right);
+                    }
+                    else if (InputMode == InputModes.GamePad)
+                    {
+                        return CurrentGamePad.IsButtonDown(GamePadScheme.Right);
+                    }
+                    break;
+
+                // Actions
+
+                case GameInputs.A1:
+                    if (InputMode == InputModes.Keyboard)
+                    {
+                        return CurrentKeyboard.IsKeyDown(KeyboardScheme.A1);
+                    }
+                    else if (InputMode == InputModes.GamePad)
+                    {
+                        return CurrentGamePad.IsButtonDown(GamePadScheme.A1);
+                    }
+                    break;
+                case GameInputs.A2:
+                    if (InputMode == InputModes.Keyboard)
+                    {
+                        return CurrentKeyboard.IsKeyDown(KeyboardScheme.A2);
+                    }
+                    else if (InputMode == InputModes.GamePad)
+                    {
+                        return CurrentGamePad.IsButtonDown(GamePadScheme.A2);
+                    }
+                    break;
+                case GameInputs.A3:
+                    if (InputMode == InputModes.Keyboard)
+                    {
+                        return CurrentKeyboard.IsKeyDown(KeyboardScheme.A3);
+                    }
+                    else if (InputMode == InputModes.GamePad)
+                    {
+                        return CurrentGamePad.IsButtonDown(GamePadScheme.A3);
+                    }
+                    break;
+                case GameInputs.A4:
+                    if (InputMode == InputModes.Keyboard)
+                    {
+                        return CurrentKeyboard.IsKeyDown(KeyboardScheme.A4);
+                    }
+                    else if (InputMode == InputModes.GamePad)
+                    {
+                        return CurrentGamePad.IsButtonDown(GamePadScheme.A4);
+                    }
+                    break;
+                case GameInputs.A5:
+                    if (InputMode == InputModes.Keyboard)
+                    {
+                        return CurrentKeyboard.IsKeyDown(KeyboardScheme.A5);
+                    }
+                    else if (InputMode == InputModes.GamePad)
+                    {
+                        return CurrentGamePad.IsButtonDown(GamePadScheme.A5);
+                    }
+                    break;
+
+                case GameInputs.Interact:
+                    if (InputMode == InputModes.Keyboard)
+                    {
+                        return CurrentKeyboard.IsKeyDown(KeyboardScheme.Interact);
+                    }
+                    else if (InputMode == InputModes.GamePad)
+                    {
+                        return CurrentGamePad.IsButtonDown(GamePadScheme.Interact);
+                    }
+                    break;
+
+                // Menus
+
+                case GameInputs.Pause:
+                    if (InputMode == InputModes.Keyboard)
+                    {
+                        return CurrentKeyboard.IsKeyDown(KeyboardScheme.Pause);
+                    }
+                    else if (InputMode == InputModes.GamePad)
+                    {
+                        return CurrentGamePad.IsButtonDown(GamePadScheme.Pause);
+                    }
+                    break;
+
+                case GameInputs.Quit:
+                    if (InputMode == InputModes.Keyboard)
+                    {
+                        return CurrentKeyboard.IsKeyDown(KeyboardScheme.Quit);
+                    }
+                    else if (InputMode == InputModes.GamePad)
+                    {
+                        return CurrentGamePad.IsButtonDown(GamePadScheme.Quit);
+                    }
+                    break;
+                // throw exception if otherwise
+            }
+            return false;
         }
     }
 }

92ef73c1afef9db90f0b89c62b0c3effdc223858
yasvisu <yasvisu@gmail.com>
2015-06-18 10:35:05 +0300

    Moved ControlManager to DromundKaasII namespace

--- DromundKaasII/DromundKaas/ControlManager.cs
+++ DromundKaasII/DromundKaas/ControlManager.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework.Input;
 
-namespace DromundKaasII.Engine
+namespace DromundKaasII
 {
-    class ControlManager
+    public class ControlManager
     {
+
     }
 }

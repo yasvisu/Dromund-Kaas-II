a153164bdb462e1b4d4ce0b2e634ad7c93e89d2f
npaunov <glupostii@abv.bg>
2015-06-18 16:31:14 +0300

    #5 Added Primal's Skills

--- DromundKaasII/DromundKaas/GameObjects/Skills/SkillManager.cs
+++ DromundKaasII/DromundKaas/GameObjects/Skills/SkillManager.cs
@@ -19,15 +19,43 @@ namespace DromundKaasII.GameObjects.Skills
         {
             // Load all skills here
             // ...
-            Skills["Fireball"] = new Skill()
-            {
-                Name = "Fireball",
-                ManaCost = 100,
-                FocusCost = 0,
-                Range = 5,
 
+
+            this.Skills["Hit"] = new Skill()
+                {
+                    Name = "Hit",
+                    ManaCost = 0,
+                    FocusCost = 5,
+                    Range = 1,
+                    TargetPermissions = SkillTargetPermissions.All
+                };
+
+            this.Skills["Throw Rock"] = new Skill()
+            {
+                Name = "Throw Rock",
+                ManaCost = 0,
+                FocusCost = 10,
+                Range = 5,
                 TargetPermissions = SkillTargetPermissions.All
             };
+            this.Skills["Huddle"] = new Skill()
+            {
+                Name = "Huddle",
+                ManaCost = 0,
+                FocusCost = 5,
+                Range = 0,
+                TargetPermissions = SkillTargetPermissions.All
+            };
+            this.Skills["Start Fire"] = new Skill()
+            {
+                Name = "Start Fire",
+                ManaCost = 10,
+                FocusCost = 10,
+                Range = 3,
+                TargetPermissions = SkillTargetPermissions.All
+            };
+            
+
 
             // Ice bolt...
             // Electric shock...

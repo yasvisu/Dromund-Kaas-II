eb6bf39b4d6f7a72ece5ef84c58ae6b571229244
yasvisu <yasvisu@gmail.com>
2015-06-19 10:25:30 +0300

    #2 Moved all game graphics from Game1 to new class PlayScreen.

--- DromundKaasII/DromundKaas/Game1.cs
+++ DromundKaasII/DromundKaas/Game1.cs
@@ -17,9 +17,6 @@ namespace DromundKaasII
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        Engine.Engine engine;
-        Task engineTask;
-        InputManager input;
 
         // Use this to map types to textures.
         Dictionary<Type, Texture2D> TypeTextures;
@@ -28,9 +25,6 @@ namespace DromundKaasII
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            this.engine = new Engine.Engine();
-            engineTask = new AsyncTimer(engine.UpdateGameState, int.MaxValue, (ulong)engine.GameState.GameDifficulty).StartAsync();
-            input = new InputManager();
         }
 
         /// <summary>
@@ -47,11 +41,6 @@ namespace DromundKaasII
             base.Initialize();
         }
 
-
-        Texture2D mytex;
-        Texture2D mychar;
-        Texture2D mytile;
-
         /// <summary>
         /// LoadContent will be called once per game and is the place to load
         /// all of your content.
@@ -60,9 +49,7 @@ namespace DromundKaasII
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            mytex = this.Content.Load<Texture2D>("Splash/dksplash");
-            mychar = this.Content.Load<Texture2D>("Actors/placeholderChar");
-            mytile = this.Content.Load<Texture2D>("Tiles/placeholderTile");
+
             ScreenManager.Instance.LoadContent(Content);
         }
 
@@ -83,28 +70,11 @@ namespace DromundKaasII
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (input.IsPressed(GameInputs.Quit))
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             ScreenManager.Instance.Update(gameTime);
-            input.UpdateInput();
-            /*
-            switch (engine.CycleCounter % 4)
-            {
-                case 0:
-                    engine.GameState.Player.PlayerInputOptions = GameInputs.Down;
-                    break;
-                case 1:
-                    engine.GameState.Player.PlayerInputOptions = GameInputs.Up;
-                    break;
-                case 2:
-                    engine.GameState.Player.PlayerInputOptions = GameInputs.Left;
-                    break;
-                case 3:
-                    engine.GameState.Player.PlayerInputOptions = GameInputs.Right;
-                    break;
-            }
-            */
+
             base.Update(gameTime);
         }
 
@@ -120,18 +90,7 @@ namespace DromundKaasII
             //spriteBatch.End();
             //spriteBatch.Begin();
             //spriteBatch.Draw(mytex, new Vector2(0, 0));
-            for (int i = 0; i < engine.GameState.Map.GetLength(0); i++)
-            {
-                for (int j = 0; j < engine.GameState.Map.GetLength(1); j++)
-                {
-                    spriteBatch.Draw(mytile, new Vector2(i * 64, j * 64));
-                }
-            }
 
-            foreach (var a in engine.GameState.Actors)
-            {
-                spriteBatch.Draw(mychar, new Vector2(a.MapPosition.X * 64, a.MapPosition.Y * 64));
-            }
             spriteBatch.End();
             base.Draw(gameTime);
         }
---
+++ DromundKaasII/DromundKaas/Graphics/PlayScreen.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DromundKaasII.Input;
+using DromundKaasII.Tools;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DromundKaasII.Graphics
+{
+    public class PlayScreen : GameScreen
+    {
+        Texture2D image;
+        string path;
+        Engine.Engine engine;
+        Task engineTask;
+        InputManager input;
+
+        Texture2D mytex;
+        Texture2D mychar;
+        Texture2D mytile;
+
+
+        // Use this to map types to textures.
+        Dictionary<Type, Texture2D> TypeTextures;
+
+        public override void LoadContent()
+        {
+
+
+            base.LoadContent();
+            path = "Splash/image1";
+            image = content.Load<Texture2D>(path);
+
+            mytex = this.content.Load<Texture2D>("Splash/dksplash");
+            mychar = this.content.Load<Texture2D>("Actors/placeholderChar");
+            mytile = this.content.Load<Texture2D>("Tiles/placeholderTile");
+
+            this.engine = new Engine.Engine();
+            engineTask = new AsyncTimer(engine.UpdateGameState, int.MaxValue, (ulong)engine.GameState.GameDifficulty).StartAsync();
+            input = new InputManager();
+        }
+
+        public override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            input.UpdateInput();
+            /*
+            switch (engine.CycleCounter % 4)
+            {
+                case 0:
+                    engine.GameState.Player.PlayerInputOptions = GameInputs.Down;
+                    break;
+                case 1:
+                    engine.GameState.Player.PlayerInputOptions = GameInputs.Up;
+                    break;
+                case 2:
+                    engine.GameState.Player.PlayerInputOptions = GameInputs.Left;
+                    break;
+                case 3:
+                    engine.GameState.Player.PlayerInputOptions = GameInputs.Right;
+                    break;
+            }
+            */
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            for (int i = 0; i < engine.GameState.Map.GetLength(0); i++)
+            {
+                for (int j = 0; j < engine.GameState.Map.GetLength(1); j++)
+                {
+                    spriteBatch.Draw(mytile, new Vector2(i * 64, j * 64));
+                }
+            }
+
+            foreach (var a in engine.GameState.Actors)
+            {
+                spriteBatch.Draw(mychar, new Vector2(a.MapPosition.X * 64, a.MapPosition.Y * 64));
+            }
+        }
+    }
+}

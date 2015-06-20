a9fe95bbc2c0070017013de22400835970785c63
yasvisu <yasvisu@gmail.com>
2015-06-20 15:57:32 +0300

    #15 Added button textures

--- DromundKaasII/DromundKaas/Graphics/OptionsScreen.cs
+++ DromundKaasII/DromundKaas/Graphics/OptionsScreen.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using DromundKaasII.Graphics.UI;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -9,9 +10,14 @@ namespace DromundKaasII.Graphics
 {
     public class OptionsScreen : GameScreen
     {
+        private List<Button> buttons;
+
         public override void LoadContent()
         {
             base.LoadContent();
+            this.buttons = new List<Button>();
+            this.buttons.Add(new Button() { Click = () => { }, Location = new Vector2(5, 5), Texture = content.Load<Texture2D>("button-sample") });
+            this.buttons.Add(new Button() { Click = () => { }, Location = new Vector2(60, 60), Texture = content.Load<Texture2D>("button-sample"), IsActive = true });
         }
 
         public override void UnloadContent()
@@ -26,7 +32,10 @@ namespace DromundKaasII.Graphics
 
         public override void Draw(SpriteBatch spriteBatch)
         {
-            
+            foreach (var button in buttons)
+            {
+                spriteBatch.Draw(button.Texture, button.Location, (button.IsActive ? Color.Red : Color.White));
+            }
         }
     }
 }
--- DromundKaasII/DromundKaas/Graphics/UI/Button.cs
+++ DromundKaasII/DromundKaas/Graphics/UI/Button.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace DromundKaasII.Graphics.UI
 {
     public class Button
     {
-        public Action Click
-        {
-            get;
-            set;
-        }
+        public Action Click { get; set; }
+        public Vector2 Location { get; set; }
+        public Texture2D Texture { get; set; }
+        public bool IsActive { get; set; }
     }
 }

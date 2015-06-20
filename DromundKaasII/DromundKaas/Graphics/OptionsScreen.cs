627ea4f7bde1457f5e187800df1c839947951d47
yasvisu <yasvisu@gmail.com>
2015-06-20 15:47:13 +0300

    #15 Buttons class and OptionsScreen method stubs.

--- DromundKaasII/DromundKaas/Graphics/OptionsScreen.cs
+++ DromundKaasII/DromundKaas/Graphics/OptionsScreen.cs
@@ -2,10 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace DromundKaasII.Graphics
 {
     public class OptionsScreen : GameScreen
     {
+        public override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        public override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            
+        }
     }
 }
---
+++ DromundKaasII/DromundKaas/Graphics/UI/Button.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DromundKaasII.Graphics.UI
+{
+    public class Button
+    {
+        public Action Click
+        {
+            get;
+            set;
+        }
+    }
+}

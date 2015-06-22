1164a17be21fd39fe90943b4653b5b9b93971bf8
yasvisu <yasvisu@gmail.com>
2015-06-22 12:18:25 +0300

    #16 Added Direction to IActor

--- DromundKaasII/DromundKaas/Interfaces/IActor.cs
+++ DromundKaasII/DromundKaas/Interfaces/IActor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using DromundKaasII.Engine;
+using DromundKaasII.GameObjects.Actors;
 using Microsoft.Xna.Framework;
 
 namespace DromundKaasII.Interfaces
@@ -10,5 +11,7 @@ namespace DromundKaasII.Interfaces
     public interface IActor
     {
         Vector2 MapPosition { get; set; }
+
+        Directions Direction { get; set; }
     }
 }

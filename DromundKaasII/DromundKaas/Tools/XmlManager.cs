d4fcf4cbba7ed0747749ca6be13ba82438755fa2
yasvisu <yasvisu@gmail.com>
2015-06-22 18:36:41 +0300

    #8 Documented XmlManager

--- DromundKaasII/DromundKaas/Tools/XmlManager.cs
+++ DromundKaasII/DromundKaas/Tools/XmlManager.cs
@@ -4,10 +4,22 @@ using System.Xml.Serialization;
 
 namespace DromundKaasII.Tools
 {
+    /// <summary>
+    /// A Xml serializer.
+    /// </summary>
+    /// <typeparam name="T">The type to serialize to / from.</typeparam>
     public class XmlManager<T>
     {
+        /// <summary>
+        /// The type to serialize from.
+        /// </summary>
         public Type Type;
 
+        /// <summary>
+        /// Deserializes an object from a file.
+        /// </summary>
+        /// <param name="Path">The path to the file.</param>
+        /// <returns>The deserialized object.</returns>
         public T Load(string Path)
         {
             T temp;
@@ -21,6 +33,11 @@ namespace DromundKaasII.Tools
             return temp;
         }
 
+        /// <summary>
+        /// Serializes an object into a file.
+        /// </summary>
+        /// <param name="Path">The path to the file.</param>
+        /// <param name="obj">The object to serialize.</param>
         public void Save(string Path, object obj)
         {
             using (TextWriter writer = new StreamWriter(Path))

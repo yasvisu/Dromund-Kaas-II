3070b0651e35f9cd4f5fd8acef7633b659ed1831
yasvisu <yasvisu@gmail.com>
2015-06-22 19:01:02 +0300

    #16 #8 Tidied and documented GameState

--- DromundKaasII/DromundKaas/Engine/GameState.cs
+++ DromundKaasII/DromundKaas/Engine/GameState.cs
@@ -1,23 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 
-using Microsoft.Xna.Framework;
-
-using DromundKaasII.Input;
-using DromundKaasII.Interfaces;
 using DromundKaasII.Engine.GameObjects;
-using DromundKaasII.Engine.GameObjects.Enums;
 using DromundKaasII.Engine.GameObjects.Actors;
+using DromundKaasII.Engine.GameObjects.Enums;
 using DromundKaasII.Engine.GameObjects.Tiles;
 using DromundKaasII.Engine.Interfaces;
+using DromundKaasII.Interfaces;
 
 namespace DromundKaasII.Engine
 {
+    /// <summary>
+    /// Represents a gamestate with all the appropriate variables.
+    /// </summary>
     public class GameState
     {
-
+        /// <summary>
+        /// Default constructor, setting a Fast game speed.
+        /// </summary>
+        /// <param name="X">The width of the game map.</param>
+        /// <param name="Y">The height of the game map.</param>
         public GameState(int X, int Y)
         {
             this.GameSpeed = GameSpeedOptions.Fast;
@@ -26,12 +27,39 @@ namespace DromundKaasII.Engine
             this.TranspiredEvents = new Queue<ActorStateEvent>();
         }
 
+        /// <summary>
+        /// The main player of this GameState.
+        /// </summary>
         public IPlayer Player { get; set; }
+
+        /// <summary>
+        /// A collection of events that have occurred.
+        /// </summary>
         public Queue<ActorStateEvent> TranspiredEvents { get; private set; }
+
+        /// <summary>
+        /// The speed option of the GameState.
+        /// </summary>
         public GameSpeedOptions GameSpeed { get; set; }
+
+        /// <summary>
+        /// The difficulty option of the GameState.
+        /// </summary>
         public GameDifficultyOptions GameDifficulty { get; set; }
+
+        /// <summary>
+        /// A list of the actors in the GameState.
+        /// </summary>
         public List<Actor> Actors { get; set; }
+
+        /// <summary>
+        /// The map of the GameState.
+        /// </summary>
         public IPathable[,] Map { get; set; }
+
+        /// <summary>
+        /// The height of the map of the GameState.
+        /// </summary>
         public int MapHeight
         {
             get
@@ -39,6 +67,10 @@ namespace DromundKaasII.Engine
                 return this.Map.GetLength(0);
             }
         }
+
+        /// <summary>
+        /// The width of the map of the GameState.
+        /// </summary>
         public int MapWidth
         {
             get

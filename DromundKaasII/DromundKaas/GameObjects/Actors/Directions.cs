86e0cda4380b7461f554b065523a70c1da1f71ee
yasvisu <yasvisu@gmail.com>
2015-06-22 11:47:23 +0300

    #14 Rearranged directions to S-N-E-W

--- DromundKaasII/DromundKaas/GameObjects/Actors/Directions.cs
+++ DromundKaasII/DromundKaas/GameObjects/Actors/Directions.cs
@@ -7,10 +7,9 @@ namespace DromundKaasII.GameObjects.Actors
 {
     public enum Directions
     {
-        East = 1,
-        West = 2,
-        North = 3,
-        South = 4
-
+        South = 1,
+        North = 2,
+        East = 3,
+        West = 4
     }
 }

cd874562c7325544ec85d45ca74ad92c0d7a9f55
npaunov <nvpaunov@gmail.com>
2015-06-28 18:01:01 +0300

    #4 Added Node!

---
+++ DromundKaasII/DromundKaas/Engine/AI/Pathfinding/Pathfinder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DromundKaasII.Engine.AI.Pathfinding
+{
+    
+    /// <summary>
+    /// This a Path finding class used to calclulate the enemy NPC's Movements
+    /// the A* algorithm is used: https://en.wikipedia.org/wiki/A*_search_algorithm
+    /// </summary>
+    public class Pathfinder
+    {
+        // Variable for the cost of each movement on the grid. Only for up/down/left/right movement
+        private double movementCost = 1;
+
+         
+
+    }
+    
+}
